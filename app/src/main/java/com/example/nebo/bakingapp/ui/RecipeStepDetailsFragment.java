c81b5cd522d373ca9d50b6dbe34af9c027ef554a
awkonecki <awkonecki.developer@gmail.com>
2018-06-19 11:16:34 -0700

    Data Binding wihtin Fragment
    So in the onCreateView now performing data binding hoping to prevent performance penalty for traversing a large layout xml graph.
    
    Runs but at the moment `this.mBinding` is still null for the instance.

--- app/src/main/java/com/example/nebo/bakingapp/ui/RecipeStepDetailsFragment.java
+++ app/src/main/java/com/example/nebo/bakingapp/ui/RecipeStepDetailsFragment.java
@@ -1,5 +1,6 @@
 package com.example.nebo.bakingapp.ui;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,13 +8,48 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+// Playing with the databinding within the fragments to see if the two play well together.
+import com.example.nebo.bakingapp.R;
+import com.example.nebo.bakingapp.databinding.FragmentRecipeStepDetailsBinding;
 
 public class RecipeStepDetailsFragment extends Fragment {
+
+    // Basic idea for using data binding with the fragment is that the View hierarchy is not
+    // traversed for manipulation, instead the class definition from the data binding provides this
+    // accessors for us.
+    private FragmentRecipeStepDetailsBinding mBinding = null;
+
     public RecipeStepDetailsFragment() {}
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = null;
+
+        if (this.mBinding == null) {
+            this.mBinding = DataBindingUtil.getBinding(
+                    inflater.inflate(
+                            R.layout.fragment_recipe_step_details,
+                            container,
+                            false));
+        }
+
+        if (this.mBinding != null) {
+            this.mBinding.tvStepDescription.setText("Some basic text.");
+            view = this.mBinding.getRoot();
+        }
+        else {
+            view = inflater.inflate(R.layout.fragment_recipe_step_details,
+                    container,
+                    false);
+            TextView textView = view.findViewById(R.id.tv_step_description);
+            textView.setText("Databinding failed, default text.");
+        }
+
+        return view;
     }
 }
