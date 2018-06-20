a55718bad1c2d0db0deab5edeb005194ed3460d1
awkonecki <awkonecki.developer@gmail.com>
2018-06-20 11:37:52 -0700

    Loader Manager Start
    Main Activity code to actually start the async task.  Oddly though complaining about a null reference.

--- app/src/main/java/com/example/nebo/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/nebo/bakingapp/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.nebo.bakingapp;
 
 import android.database.Cursor;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.example.nebo.bakingapp.async.NetworkAsyncTask;
+import com.example.nebo.bakingapp.async.NetworkAsyncTaskLoader;
 import com.example.nebo.bakingapp.data.Data;
+import com.example.nebo.bakingapp.data.Recipe;
 import com.example.nebo.bakingapp.ui.RecipeStepDetailsFragment;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -21,6 +28,25 @@ public class MainActivity extends AppCompatActivity {
 
         manager.beginTransaction().add(R.id.fl_recipe_step_details, fragment).commit();
 
-        Log.d("Count of Recipes", Integer.toString(Data.getRecipes().length));
+        // NetworkAsyncTaskLoader taskLoader = new NetworkAsyncTaskLoader(this);
+
+        LoaderManager loaderManager = getSupportLoaderManager();
+
+        if (loaderManager != null) {
+            Loader<List<Recipe>> loader = loaderManager.getLoader(NetworkAsyncTaskLoader.QUERY_TASK_ID);
+
+            if (loader == null) {
+                loaderManager.initLoader(NetworkAsyncTaskLoader.QUERY_TASK_ID,
+                        null,
+                        new NetworkAsyncTaskLoader(this)).forceLoad();
+            } else {
+                //loaderManager.restartLoader(NetworkAsyncTaskLoader.QUERY_TASK_ID,
+                //        null,
+                //        new NetworkAsyncTaskLoader(this)).forceLoad();
+            }
+        }
+        else {
+            Log.e("MainActivity", "Loader Manager is null.");
+        }
     }
 }
--- app/src/main/java/com/example/nebo/bakingapp/async/NetworkAsyncTaskLoader.java
+++ app/src/main/java/com/example/nebo/bakingapp/async/NetworkAsyncTaskLoader.java
@@ -1,21 +1,33 @@
 package com.example.nebo.bakingapp.async;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.util.Log;
 
-public class NetworkAsyncTaskLoader implements LoaderManager.LoaderCallbacks<String> {
+import com.example.nebo.bakingapp.data.Recipe;
+
+import java.util.List;
+
+public class NetworkAsyncTaskLoader implements LoaderManager.LoaderCallbacks<List<Recipe>> {
     public static final int QUERY_TASK_ID = 1;
+    private final Context mContext;
+
+    public NetworkAsyncTaskLoader(Context context) {
+        this.mContext = context;
+    }
 
     @NonNull
     @Override
-    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
-        Loader<String> loader = null;
+    public Loader<List<Recipe>> onCreateLoader(int id, @Nullable Bundle args) {
+        Loader<List<Recipe>> loader = null;
 
         switch(id) {
-
+            case QUERY_TASK_ID:
+                loader = new NetworkAsyncTask(this.mContext);
             default:
                 break;
         }
@@ -24,10 +36,10 @@ public class NetworkAsyncTaskLoader implements LoaderManager.LoaderCallbacks<Str
     }
 
     @Override
-    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
-
+    public void onLoadFinished(@NonNull Loader<List<Recipe>> loader, List<Recipe> data) {
+        Log.d("Data Size", Integer.toString(data.size()));
     }
 
     @Override
-    public void onLoaderReset(@NonNull Loader<String> loader) {}
+    public void onLoaderReset(@NonNull Loader<List<Recipe>> loader) {}
 }
