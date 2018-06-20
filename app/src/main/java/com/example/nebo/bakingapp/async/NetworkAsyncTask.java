b98f6427140f28013542e6e99d530e122bb330f6
awkonecki <awkonecki.developer@gmail.com>
2018-06-20 11:13:41 -0700

    Retrofit 2 Network Task
    So built a basic callback for working with retrofit within the background task to fetch the JSON data that GSON will convert to a List of recipes for me.

--- app/src/main/java/com/example/nebo/bakingapp/async/NetworkAsyncTask.java
+++ app/src/main/java/com/example/nebo/bakingapp/async/NetworkAsyncTask.java
@@ -6,11 +6,24 @@ import android.support.annotation.Nullable;
 import android.support.v4.content.AsyncTaskLoader;
 import android.util.Log;
 
-import java.net.URL;
+import com.example.nebo.bakingapp.data.Recipe;
 
-public class NetworkAsyncTask extends AsyncTaskLoader<String> {
+import java.net.URL;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+public class NetworkAsyncTask extends AsyncTaskLoader<List<Recipe>> {
     private static final String sEndPointAddress =
             "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
+
+    private List<Recipe> mRecipes;
 
     public NetworkAsyncTask(Context context) {
         super(context);
@@ -41,8 +54,34 @@ public class NetworkAsyncTask extends AsyncTaskLoader<String> {
 
     @Nullable
     @Override
-    public String loadInBackground() {
+    public List<Recipe> loadInBackground() {
 
-        return null;
+        Retrofit retrofit = new Retrofit.Builder().
+                baseUrl(sEndPointAddress).
+                addConverterFactory(GsonConverterFactory.create()).
+                client(new OkHttpClient()).
+                build();
+        RecipeService client = retrofit.create(RecipeService.class);
+
+        Call<List<Recipe>> call = client.listRecipes();
+
+        call.enqueue(new Callback<List<Recipe>>() {
+            @Override
+            public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
+                mRecipes = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<List<Recipe>> call, Throwable t) {
+                mRecipes = null;
+            }
+        });
+
+        return mRecipes;
+    }
+
+    public interface RecipeService {
+        @GET("")
+        Call<List<Recipe>> listRecipes();
     }
 }
