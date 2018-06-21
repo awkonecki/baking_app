519b378a8984487ffd13dec6978c279f9643bb82
awkonecki <awkonecki.developer@gmail.com>
2018-06-21 10:52:54 -0700

    Formal Activity - Baking Activity
    So instead of using just the default `MainActivity` class, I will be replacing it with a new more context driven name of `BakingActivity`.

--- app/src/main/java/com/example/nebo/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/nebo/bakingapp/MainActivity.java
@@ -1,19 +1,10 @@
 package com.example.nebo.bakingapp;
 
-import android.database.Cursor;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
-import com.example.nebo.bakingapp.async.NetworkAsyncTask;
-import com.example.nebo.bakingapp.async.NetworkAsyncTaskLoader;
-import com.example.nebo.bakingapp.data.Data;
 import com.example.nebo.bakingapp.data.Recipe;
 import com.example.nebo.bakingapp.ui.RecipeStepDetailsFragment;
 import com.example.nebo.bakingapp.util.NetworkUtils;
@@ -23,8 +14,6 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-
-import static com.example.nebo.bakingapp.async.NetworkAsyncTaskLoader.QUERY_TASK_ID;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MAIN ACTIVITY";
