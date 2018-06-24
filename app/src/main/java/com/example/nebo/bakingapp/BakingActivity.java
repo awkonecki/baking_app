9fcf802868cadc0c9b8f49145b0413157df2b34b
awkonecki <awkonecki.developer@gmail.com>
2018-06-23 21:38:48 -0700

    Baking Activity
    Replacing the `MainActivity` with the `BakingActivity` as the primary top-application that gets loaded.
    
    - Added the necessary layout file that will be the view for the `BakingActivity`.
    - DataBinding initial pieces in places as well.

--- app/src/main/java/com/example/nebo/bakingapp/BakingActivity.java
+++ app/src/main/java/com/example/nebo/bakingapp/BakingActivity.java
@@ -1,6 +1,22 @@
 package com.example.nebo.bakingapp;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.nebo.bakingapp.databinding.ActivityBakingBinding;
+
 public class BakingActivity extends AppCompatActivity {
+    private static ActivityBakingBinding mBinding = null;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        BakingActivity.mBinding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_baking);
+
+
+    }
 }
