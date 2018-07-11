bc236ba3f2b4efd6d757a32c9cd33e06fe26ff3b
Andrew Konecki <awkonecki.developer@gmail.com>
2018-07-11 13:03:49 -0700

    [FTR] Espessro Test Baking Activity Content
    Feature Development
    
    Test for checking the content of the recycler view of each recipe in the
    expected position exists now works as expected.
    
    Related Issues
    - #43

--- app/src/androidTest/java/com/example/nebo/bakingapp/BakingActivityTest.java
+++ app/src/androidTest/java/com/example/nebo/bakingapp/BakingActivityTest.java
@@ -1,12 +1,16 @@
 package com.example.nebo.bakingapp;
 
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -27,6 +31,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -51,24 +56,44 @@ public class BakingActivityTest {
         // RecipeActivity should now be viewable.
         onView(withId(R.id.ll_recipe)).check(doesNotExist());
 
-        // check to see if the recycler view is already displayed.
+        // check to see if the frame layout that is supposed to contain the recycler view is already displayed.
         onView(withId(R.id.fl_recipes)).check(matches(isDisplayed()));
-        onView(atRecyclerViewPosition(allOf(withText(R.string.recipe_one)), 0)).check(matches(withText(R.string.recipe_one)));
+
+        // Make sure that each recipe is in the correct position.
+        for (int position = 0; position < 4; position++) {
+            String recipeName = null;
+            int id = 0;
+
+            switch(position) {
+                case 0:
+                    id = R.string.recipe_one;
+                    break;
+                case 1:
+                    id = R.string.recipe_two;
+                    break;
+                case 2:
+                    id = R.string.recipe_three;
+                    break;
+                case 3:
+                    id = R.string.recipe_four;
+                    break;
+            }
+
+            recipeName = InstrumentationRegistry.getTargetContext().getResources().getString(id);
+
+            for (int subposition = 0; subposition < 4; subposition++) {
 
 
-        // .perform(RecyclerViewActions.<RecipeViewHolder<Recipe>>scrollToPosition(0)).;
-
-
-
-        // onView(withId(R.id.fl_recipes)).+
-        // onView(withId(R.id.fl_recipes)).perform(RecyclerViewActions.<RecipeViewHolder<Recipe>>actionOnItemAtPosition(2, click()));
-
-        // onView(withId(R.id.fl_ingredients)).check(matches((isDisplayed())));
-    }
-
-    @Test
-    public void hasRecipeNutellaPie() {
-
+                if (subposition == position) {
+                    // Positive case for existence.
+                    onView(atPositionOnView(subposition, -1, R.id.rv_recipes)).check(matches(hasDescendant(withText(recipeName))));
+                }
+                else {
+                    // Negative case for not equal
+                    onView(atPositionOnView(subposition, -1, R.id.rv_recipes)).check(matches(not(hasDescendant(withText(recipeName)))));
+                }
+            }
+        }
     }
 
     @Test
@@ -88,20 +113,66 @@ public class BakingActivityTest {
         }
     }
 
-    @NonNull
-    private static Matcher<View> atRecyclerViewPosition(final Matcher<View> parentMatcher, final int position) {
+
+    private static Matcher<View> atPositionOnView(final int position, final int targetViewId, final int recyclerViewId) {
         return new TypeSafeMatcher<View>() {
+            Resources resources = null;
+            View childView = null;
+
             @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
+            protected boolean matchesSafely(View item) {
+                resources = item.getResources();
+
+                if (childView == null) {
+                    RecyclerView recyclerView = (RecyclerView) item.getRootView().findViewById(recyclerViewId);
+
+                    if (recyclerView != null && recyclerView.getId() == recyclerViewId) {
+                        RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForAdapterPosition(position);
+                        if (viewHolder != null) {
+                            childView = viewHolder.itemView;
+                        }
+                    }
+                    else {
+                        return false;
+                    }
+                }
+
+                if (targetViewId == -1) {
+                    return item == childView;
+                }
+                else {
+                    View targetView = childView.findViewById(targetViewId);
+                    return item == targetView;
+                }
             }
 
             @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            public void describeTo(Description description) {
+
+            }
+        };
+    }
+
+    // stackoverflow.com/questions/31394569/how-to-assert-inside-a-recyclerview-in-espresso
+    @NonNull
+    private static Matcher<View> atRecyclerPosition(@NonNull final Matcher<View> itemMatcher, final int position) {
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("item at position " + Integer.toString(position) + " is ");
+                itemMatcher.describeTo(description);
+            }
+
+            @Override
+            protected boolean matchesSafely(RecyclerView item) {
+                RecyclerView.ViewHolder viewHolder = item.findViewHolderForAdapterPosition(position);
+
+                if (viewHolder != null) {
+                    return false;
+                }
+                else {
+                    return itemMatcher.matches(viewHolder.itemView);
+                }
             }
         };
     }
---
+++ app/src/androidTest/java/com/example/nebo/bakingapp/BakingActivityTest3.java
@@ -0,0 +1,104 @@
+package com.example.nebo.bakingapp;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class BakingActivityTest3 {
+
+    @Rule
+    public ActivityTestRule<BakingActivity> mActivityTestRule = new ActivityTestRule<>(BakingActivity.class);
+
+    @Test
+    public void bakingActivityTest3() {
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.rv_recipes),
+                        childAtPosition(
+                                withClassName(is("android.support.constraint.ConstraintLayout")),
+                                0)));
+        recyclerView.perform(actionOnItemAtPosition(0, click()));
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.action_bar),
+                                        childAtPosition(
+                                                withId(R.id.action_bar_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction recyclerView2 = onView(
+                allOf(withId(R.id.rv_recipes),
+                        childAtPosition(
+                                withClassName(is("android.support.constraint.ConstraintLayout")),
+                                0)));
+        recyclerView2.perform(actionOnItemAtPosition(3, click()));
+
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.action_bar),
+                                        childAtPosition(
+                                                withId(R.id.action_bar_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton2.perform(click());
+
+        ViewInteraction linearLayout = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.rv_recipes),
+                                0),
+                        0),
+                        isDisplayed()));
+        linearLayout.check(matches(isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
