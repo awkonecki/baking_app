acf98205b2bd00e9dfa13eb6f46f329ff7577329
Andrew Konecki <awkonecki.developer@gmail.com>
2018-07-10 21:43:02 -0700

    [FTR] Android ViewMatchers
    Feature Development
    
    Trying to test that the current set of recipes exist within the recipe
    recycler view in specific positions (order in which they are available
    from the network).  Referencing the definition from the espresso
    recording session.
    
    Still a work in progress.

--- app/src/androidTest/java/com/example/nebo/bakingapp/BakingActivityTest.java
+++ app/src/androidTest/java/com/example/nebo/bakingapp/BakingActivityTest.java
@@ -1,14 +1,22 @@
 package com.example.nebo.bakingapp;
 
+import android.support.annotation.NonNull;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import com.example.nebo.bakingapp.data.Recipe;
 import com.example.nebo.bakingapp.viewholder.RecipeViewHolder;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -17,10 +25,12 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.not;
 
 @RunWith(AndroidJUnit4.class)
@@ -37,12 +47,18 @@ public class BakingActivityTest {
     }
 
     @Test
-    public void hasRecipeBrownie() {
-        // Something about a footer not being displayed first, dont know what that is.
-
+    public void hasRecipes() {
+        // RecipeActivity should now be viewable.
+        onView(withId(R.id.ll_recipe)).check(doesNotExist());
 
         // check to see if the recycler view is already displayed.
         onView(withId(R.id.fl_recipes)).check(matches(isDisplayed()));
+        onView(atRecyclerViewPosition(allOf(withText(R.string.recipe_one)), 0)).check(matches(withText(R.string.recipe_one)));
+
+
+        // .perform(RecyclerViewActions.<RecipeViewHolder<Recipe>>scrollToPosition(0)).;
+
+
 
         // onView(withId(R.id.fl_recipes)).+
         // onView(withId(R.id.fl_recipes)).perform(RecyclerViewActions.<RecipeViewHolder<Recipe>>actionOnItemAtPosition(2, click()));
@@ -70,5 +86,23 @@ public class BakingActivityTest {
         if (mIdlingResource != null) {
             Espresso.unregisterIdlingResources(mIdlingResource);
         }
+    }
+
+    @NonNull
+    private static Matcher<View> atRecyclerViewPosition(final Matcher<View> parentMatcher, final int position) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
